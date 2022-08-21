913f3615d53d82f0910fddcde9d4074a296ff678
Sakil Ahammad Laskar <sakilahammadlaskar786@gmail.com>
2022-08-21 11:04:46 +0530

    Initial commit

---
+++ src/com/coder/Login.java
@@ -0,0 +1,63 @@
+package com.coder;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		
+		String name=request.getParameter("name");
+		String email=request.getParameter("email");
+		String password=request.getParameter("pwd");
+		
+		
+		if(password.equals("Admin123")&& email.equalsIgnoreCase("admin@123"))
+		{
+			RequestDispatcher requestDispatcher=request.getRequestDispatcher("welcomepage");
+			requestDispatcher.forward(request, response);
+		}
+		else
+		{
+			pw.print("Email or Password is wrong");
+			RequestDispatcher requestDispatcher=request.getRequestDispatcher("/index.html");
+			requestDispatcher.include(request, response);
+		}
+		
+	}
+
+}
